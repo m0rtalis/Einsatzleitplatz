a8310865f2006521f6a5d0fc9ff5bd8707622a92
Fabian <fabian.eisinger@gmx.de>
2024-02-17 23:20:17 +0100

    Initial commit

---
+++ src/main/java/de/eisingerf/elp/ElpApplication.java
@@ -0,0 +1,13 @@
+package de.eisingerf.elp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElpApplication.class, args);
+    }
+
+}
